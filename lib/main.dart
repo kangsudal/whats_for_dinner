7520315113ab6a535fd8ccb8a0dbec68e67ab0a1
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-07-12 23:58:28 +0900

    Fetch json data and convert to object

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:whats_for_dinner/screen/homeScreen.dart';
+import 'package:http/http.dart' as http;
+
+import 'model/food.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +13,27 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    fetchAlbum();
     return MaterialApp(
       home: HomeScreen(),
     );
   }
 }
 
+void fetchAlbum() async {
+  final response =
+  await http.get(Uri.parse('http://openapi.foodsafetykorea.go.kr/api/sample/COOKRCP01/json/1/5'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    var jsonList = jsonDecode(response.body)["COOKRCP01"]["row"];
+    print(jsonList.map((json)=>Food.fromJson(json)).toList()[0].runtimeType);
+    print(jsonList.map((json)=>Food.fromJson(json)).toList()[0].rcpnm);
+//    return Food.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load data');
+  }
+}
