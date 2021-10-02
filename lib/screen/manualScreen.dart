0b3f1ff39bc3deab7e7813dae623542ed266e7c3
kangsudal <32862869+kangsudal@users.noreply.github.com>
2021-10-03 00:14:51 +0900

    레시피 설명글씨부분 까맣게 나오는거 수정. 상단에 완성요리사진 추가.

--- lib/screen/manualScreen.dart
+++ lib/screen/manualScreen.dart
@@ -49,86 +49,139 @@ class _ManualScreenState extends State<ManualScreen> {
         child: RepaintBoundary(
           //캡쳐하고싶은 위젯 범위
           key: previewContainer,
-          child: Column(
+          child: Stack(
             children: [
-              SizedBox(
-                height: 20,
+              Positioned(
+                child: Container(
+                  color: Colors.white,
+                ),
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0,
               ),
-              Text("재료: ${widget.recipe.rcppartsdtls!}"),
-              SizedBox(
-                height: 10,
-              ),
-              if (widget.recipe.manual01 != '') Text(widget.recipe.manual01!),
-              if (widget.recipe.manualimg01 != '')
-                Image.network(widget.recipe.manualimg01!),
-              if (widget.recipe.manual02 != '') Text(widget.recipe.manual02!),
-              if (widget.recipe.manualimg02 != '')
-                Image.network(widget.recipe.manualimg02!),
-              if (widget.recipe.manual03 != '') Text(widget.recipe.manual03!),
-              if (widget.recipe.manualimg03 != '')
-                Image.network(widget.recipe.manualimg03!),
-              if (widget.recipe.manual04 != '') Text(widget.recipe.manual04!),
-              if (widget.recipe.manualimg04 != '')
-                Image.network(widget.recipe.manualimg04!),
-              if (widget.recipe.manual05 != '') Text(widget.recipe.manual05!),
-              if (widget.recipe.manualimg05 != '')
-                Image.network(widget.recipe.manualimg05!),
-              if (widget.recipe.manual06 != '') Text(widget.recipe.manual06!),
-              if (widget.recipe.manualimg06 != '')
-                Image.network(widget.recipe.manualimg06!),
-              if (widget.recipe.manual07 != '') Text(widget.recipe.manual07!),
-              if (widget.recipe.manualimg07 != '')
-                Image.network(widget.recipe.manualimg07!),
-              if (widget.recipe.manual08 != '') Text(widget.recipe.manual08!),
-              if (widget.recipe.manualimg08 != '')
-                Image.network(widget.recipe.manualimg08!),
-              if (widget.recipe.manual09 != '') Text(widget.recipe.manual09!),
-              if (widget.recipe.manualimg09 != '')
-                Image.network(widget.recipe.manualimg09!),
-              if (widget.recipe.manual10 != '') Text(widget.recipe.manual10!),
-              if (widget.recipe.manualimg10 != '')
-                Image.network(widget.recipe.manualimg10!),
-              if (widget.recipe.manual11 != '') Text(widget.recipe.manual11!),
-              if (widget.recipe.manualimg11 != '')
-                Image.network(widget.recipe.manualimg11!),
-              if (widget.recipe.manual12 != '') Text(widget.recipe.manual12!),
-              if (widget.recipe.manualimg12 != '')
-                Image.network(widget.recipe.manualimg12!),
-              if (widget.recipe.manual13 != '') Text(widget.recipe.manual13!),
-              if (widget.recipe.manualimg13 != '')
-                Image.network(widget.recipe.manualimg13!),
-              if (widget.recipe.manual14 != '') Text(widget.recipe.manual14!),
-              if (widget.recipe.manualimg14 != '')
-                Image.network(widget.recipe.manualimg14!),
-              if (widget.recipe.manual15 != '') Text(widget.recipe.manual15!),
-              if (widget.recipe.manualimg15 != '')
-                Image.network(widget.recipe.manualimg15!),
-              if (widget.recipe.manual16 != '') Text(widget.recipe.manual16!),
-              if (widget.recipe.manualimg16 != '')
-                Image.network(widget.recipe.manualimg16!),
-              if (widget.recipe.manual17 != '') Text(widget.recipe.manual17!),
-              if (widget.recipe.manualimg17 != '')
-                Image.network(widget.recipe.manualimg17!),
-              if (widget.recipe.manual18 != '') Text(widget.recipe.manual18!),
-              if (widget.recipe.manualimg18 != '')
-                Image.network(widget.recipe.manualimg18!),
-              if (widget.recipe.manual19 != '') Text(widget.recipe.manual19!),
-              if (widget.recipe.manualimg19 != '')
-                Image.network(widget.recipe.manualimg19!),
-              if (widget.recipe.manual20 != '') Text(widget.recipe.manual20!),
-              if (widget.recipe.manualimg20 != '')
-                Image.network(widget.recipe.manualimg20!),
-              ElevatedButton(
-                child: Text("오늘은 이 메뉴로 확정!"),
-                onPressed: () {
+              Column(
+                children: [
+                  Container(
+                    child: ClipRRect(
+                      child: Image.network(widget.recipe.attfilenomain!),
+                      borderRadius: BorderRadius.circular(30),
+
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(45),
+                        bottomRight: Radius.circular(45),
+                      ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3),
+                          ),
+                        ]),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("재료: ${widget.recipe.rcppartsdtls!}"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  if (widget.recipe.manual01 != '')
+                    Text(widget.recipe.manual01!),
+                  if (widget.recipe.manualimg01 != '')
+                    Image.network(widget.recipe.manualimg01!),
+                  if (widget.recipe.manual02 != '')
+                    Text(widget.recipe.manual02!),
+                  if (widget.recipe.manualimg02 != '')
+                    Image.network(widget.recipe.manualimg02!),
+                  if (widget.recipe.manual03 != '')
+                    Text(widget.recipe.manual03!),
+                  if (widget.recipe.manualimg03 != '')
+                    Image.network(widget.recipe.manualimg03!),
+                  if (widget.recipe.manual04 != '')
+                    Text(widget.recipe.manual04!),
+                  if (widget.recipe.manualimg04 != '')
+                    Image.network(widget.recipe.manualimg04!),
+                  if (widget.recipe.manual05 != '')
+                    Text(widget.recipe.manual05!),
+                  if (widget.recipe.manualimg05 != '')
+                    Image.network(widget.recipe.manualimg05!),
+                  if (widget.recipe.manual06 != '')
+                    Text(widget.recipe.manual06!),
+                  if (widget.recipe.manualimg06 != '')
+                    Image.network(widget.recipe.manualimg06!),
+                  if (widget.recipe.manual07 != '')
+                    Text(widget.recipe.manual07!),
+                  if (widget.recipe.manualimg07 != '')
+                    Image.network(widget.recipe.manualimg07!),
+                  if (widget.recipe.manual08 != '')
+                    Text(widget.recipe.manual08!),
+                  if (widget.recipe.manualimg08 != '')
+                    Image.network(widget.recipe.manualimg08!),
+                  if (widget.recipe.manual09 != '')
+                    Text(widget.recipe.manual09!),
+                  if (widget.recipe.manualimg09 != '')
+                    Image.network(widget.recipe.manualimg09!),
+                  if (widget.recipe.manual10 != '')
+                    Text(widget.recipe.manual10!),
+                  if (widget.recipe.manualimg10 != '')
+                    Image.network(widget.recipe.manualimg10!),
+                  if (widget.recipe.manual11 != '')
+                    Text(widget.recipe.manual11!),
+                  if (widget.recipe.manualimg11 != '')
+                    Image.network(widget.recipe.manualimg11!),
+                  if (widget.recipe.manual12 != '')
+                    Text(widget.recipe.manual12!),
+                  if (widget.recipe.manualimg12 != '')
+                    Image.network(widget.recipe.manualimg12!),
+                  if (widget.recipe.manual13 != '')
+                    Text(widget.recipe.manual13!),
+                  if (widget.recipe.manualimg13 != '')
+                    Image.network(widget.recipe.manualimg13!),
+                  if (widget.recipe.manual14 != '')
+                    Text(widget.recipe.manual14!),
+                  if (widget.recipe.manualimg14 != '')
+                    Image.network(widget.recipe.manualimg14!),
+                  if (widget.recipe.manual15 != '')
+                    Text(widget.recipe.manual15!),
+                  if (widget.recipe.manualimg15 != '')
+                    Image.network(widget.recipe.manualimg15!),
+                  if (widget.recipe.manual16 != '')
+                    Text(widget.recipe.manual16!),
+                  if (widget.recipe.manualimg16 != '')
+                    Image.network(widget.recipe.manualimg16!),
+                  if (widget.recipe.manual17 != '')
+                    Text(widget.recipe.manual17!),
+                  if (widget.recipe.manualimg17 != '')
+                    Image.network(widget.recipe.manualimg17!),
+                  if (widget.recipe.manual18 != '')
+                    Text(widget.recipe.manual18!),
+                  if (widget.recipe.manualimg18 != '')
+                    Image.network(widget.recipe.manualimg18!),
+                  if (widget.recipe.manual19 != '')
+                    Text(widget.recipe.manual19!),
+                  if (widget.recipe.manualimg19 != '')
+                    Image.network(widget.recipe.manualimg19!),
+                  if (widget.recipe.manual20 != '')
+                    Text(widget.recipe.manual20!),
+                  if (widget.recipe.manualimg20 != '')
+                    Image.network(widget.recipe.manualimg20!),
+                  ElevatedButton(
+                    child: Text("오늘은 이 메뉴로 확정!"),
+                    onPressed: () {
 //                print("똑똑 열리셨나요?${Hive.isBoxOpen('eatNoteBox')}");
-                  final Box box = Hive.box<EatNote>('eatNoteBox');
-                  box.add(EatNote(
-                      recipe: widget.recipe, eatDateTime: DateTime.now()));
-                  Navigator.of(context).pop();
-                },
+                      final Box box = Hive.box<EatNote>('eatNoteBox');
+                      box.add(EatNote(
+                          recipe: widget.recipe, eatDateTime: DateTime.now()));
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  SizedBox(height: 20),
+                ],
               ),
-              SizedBox(height: 20),
             ],
           ),
         ),
