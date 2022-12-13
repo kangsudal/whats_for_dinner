2f141c98276ea8e54dd3f6b6e737c45abb7ad63e
kangsudal_newcomputer <kangsudal@gmail.com>
2022-12-13 14:13:55 +0900

    TextField를 이용한 검색기능 완료

--- lib/screen/restaurantScreen.dart
+++ lib/screen/restaurantScreen.dart
@@ -23,6 +23,7 @@ class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
   void initState() {
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,25 +82,36 @@ class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
       return '앱의 위치 권한을 세팅에서 허가해주세요.';
     }
     //위에 아무것도 안걸리고 모든게 통과되면
-    ref.read(searchTextProvider.notifier).update((state) => widget.recipe.rcpnm!);//이 위치에 넣어줘야한다 중요 ★★★★★★ FutureBuilder가 위젯트리를 생성하기 전에!
+    ref.read(searchTextProvider.notifier).update((state) => widget
+        .recipe.rcpnm!); //이 위치에 넣어줘야한다 중요 ★★★★★★ FutureBuilder가 위젯트리를 생성하기 전에!
     return '위치 권한이 허가되었습니다.';
   }
 }
 
-class SearchField extends StatelessWidget {
+class SearchField extends ConsumerStatefulWidget {
   final Recipe recipe;
-  const SearchField({
+
+  SearchField({
     Key? key,
     required this.recipe,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    String keyword = recipe.rcpnm!;
-    String hashTag = recipe.hashtag!;
+  ConsumerState<SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends ConsumerState<SearchField> {
+  late TextEditingController controller1;
+  late TextEditingController controller2;
+
+  @override
+  void initState() {
+    super.initState();
+    String keyword = widget.recipe.rcpnm!;
+    String hashTag = widget.recipe.hashtag!;
     String ingredient1;
     String ingredient2;
-    List<String> rcppartsdtls = recipe.rcppartsdtls!.split('\n');
+    List<String> rcppartsdtls = widget.recipe.rcppartsdtls!.split('\n');
     if (rcppartsdtls[0].contains(',')) {
       ingredient1 = rcppartsdtls[0].split(',')[0];
       ingredient2 = rcppartsdtls[0].split(',')[1];
@@ -107,7 +119,18 @@ class SearchField extends StatelessWidget {
       ingredient1 = rcppartsdtls[0];
       ingredient2 = rcppartsdtls[1].split(',')[0];
     }
+    hashTag = (hashTag == "") ? '$ingredient1 $ingredient2' : hashTag;
+    controller1 = TextEditingController(text: keyword);
+    controller2 = TextEditingController(text: hashTag);
+  }
 
+  void submitData(String text) {
+    ref.read(searchTextProvider.notifier).update((state) => text);
+    print('=======UPDATED textfield: $text=======');
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
       height: 130,
       child: Column(
@@ -115,22 +138,31 @@ class SearchField extends StatelessWidget {
           Row(
             children: [
               Expanded(
-                child: TextFormField(
-                  initialValue: keyword,
+                child: TextField(
+                  controller: controller1,
+                  onSubmitted: submitData,
                 ),
               ),
-              TextButton(onPressed: () {}, child: Text('검색')),
+              TextButton(
+                  onPressed: () {
+                    submitData(controller1.text);
+                  },
+                  child: Text('검색')),
             ],
           ),
           Row(
             children: [
               Expanded(
-                child: TextFormField(
-                  initialValue:
-                      hashTag == "" ? '$ingredient1 $ingredient2' : hashTag,
+                child: TextField(
+                  controller: controller2,
+                  onSubmitted: submitData,
                 ),
               ),
-              TextButton(onPressed: () {}, child: Text('검색')),
+              TextButton(
+                  onPressed: () {
+                    submitData(controller2.text);
+                  },
+                  child: Text('검색')),
             ],
           ),
           SizedBox(height: 10),
@@ -161,14 +193,16 @@ class CustomGoogleMap extends ConsumerWidget {
             ),
           );
         }
-        return Expanded(child: GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: data!.currentPosition,
-            zoom: 12,
+        return Expanded(
+          child: GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: data!.currentPosition,
+              zoom: 12,
+            ),
+            // onMapCreated: mapCreated,
+            markers: Set.from(data!.markers),
           ),
-          // onMapCreated: mapCreated,
-          markers: Set.from(data!.markers),
-        ),);
+        );
       },
       error: (error, stack) {
         return Expanded(
@@ -176,9 +210,11 @@ class CustomGoogleMap extends ConsumerWidget {
             child: Text('마땅한 식당이 없습니다. ㅠㅠ 검색어를 수정해서 찾아보실래요?'),
           ),
         );
-        return Expanded(child: Center(
-          child: Text(error.toString()),
-        ),);
+        return Expanded(
+          child: Center(
+            child: Text(error.toString()),
+          ),
+        );
       },
       loading: () => Expanded(
         child: Center(
@@ -186,7 +222,5 @@ class CustomGoogleMap extends ConsumerWidget {
         ),
       ),
     );
-
   }
-
 }
